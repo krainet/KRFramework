86c5aa489393c3df33cf67b2422152398c980aa0
Ramón Albertí Danés <krainet@gmail.com>
2014-03-29 22:55:36 +0100

    Finishing Base of Framework

--- libs/KrAutoload.php
+++ libs/KrAutoload.php
@@ -17,8 +17,9 @@ function __autoload($className) {
         require_once( MODELS_PATH . "/$className.php" );
     } elseif (strpos($className, '_controller')) {
         $className = str_replace("_", ".", $className);
-        if(!strpos($className, 'favicon'))
-            require_once( CONTROLLERS_PATH . "/$className.php" );
+        error_log('NombreClase::'.$className);
+        
+        require_once( CONTROLLERS_PATH . "/$className.php" );
     } elseif (strpos($className, 'Fw_')) {
         $className = str_replace("_", ".", $className);
         require_once( CLASS_PATH_FW . "/$className.php" );        
