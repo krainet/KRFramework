92d09c3be233a75c0239423fdc91df4de82be0d4
Ramón Albertí Danés <krainet@gmail.com>
2014-03-30 15:09:02 +0200

    Finishing Base of Framework

--- public/index.php
+++ public/index.php
@@ -18,6 +18,7 @@ define('CONFIG_PATH', APP_PATH . DS . 'config');
 require_once CONFIG_PATH . '/defines.php';
 require_once LIBS_PATH . '/KrAutoload.php';
 
+$Init = new AppInit();
 
 if (!Tools_Fw::FileExistsUri($_SERVER['REQUEST_URI'])) {
     $Route = new Route_Fw();
