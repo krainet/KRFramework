f055b1baa8771e9bb7300af6289b5d7303ee1f66
Ramón Albertí Danés <krainet@gmail.com>
2014-04-05 22:55:12 +0200

    Adding CRUD System & Others

---
+++ app/modules/ajax_execution.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Users Module
+ * @author Ramón Albertí
+ */
+?>
+
+
+&nbsp;<p>
+<div class="container theme-showcase" role="main">
+
+    &nbsp;<p>
+
+    <div class="well">
+        <form name="ftest" method="post" action="ajax/call">
+            <input type="text" name="prueba" value=""><input type="submit" value="Enviar">
+        </form>
+    </div>
+        
+        
+    <div class="alert alert-success">
+        <strong>KR Framework</strong> Ajax Module Loaded!.
+    </div>
+
+    <div class="well">
+        <p>Krainet.NET - Ramón Albertí Danés (krainet@gmail.com)</p>
+    </div>
+
+
+</div> <!-- /container -->
+
+
+<?php Modules_Fw::getModule('ajax/users'); ?>
+
