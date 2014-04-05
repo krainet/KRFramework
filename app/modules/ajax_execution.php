2bcf96a9e85c06906f53d6abff677482b9ad4154
Ramón Albertí Danés <krainet@gmail.com>
2014-04-05 22:57:22 +0200

    Adding CRUD System & Others

--- app/modules/ajax_execution.php
+++ app/modules/ajax_execution.php
@@ -12,7 +12,7 @@
     &nbsp;<p>
 
     <div class="well">
-        <form name="ftest" method="post" action="self">
+        <form name="ftest" method="post" action="_self">
             <input type="text" name="prueba" value=""><input type="submit" value="Enviar">
         </form>
     </div>
